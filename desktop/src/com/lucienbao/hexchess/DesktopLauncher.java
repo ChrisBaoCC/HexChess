4373dc192f36fe49e0a39db4643354a1f4c5fb36
Lucien Bao <lucien.bao@proton.me>
2023-08-05 21:05:02 -0400

    Test commit

--- desktop/src/com/lucienbao/hexchess/DesktopLauncher.java
+++ desktop/src/com/lucienbao/hexchess/DesktopLauncher.java
@@ -8,8 +8,9 @@ import com.lucienbao.hexchess.HexChess;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(60);
 		config.setTitle("HexChess");
+		config.setWindowedMode(1920, 1080);
+		config.setForegroundFPS(60);
 		new Lwjgl3Application(new HexChess(), config);
 	}
 }
